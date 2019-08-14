d631d82e375fc270979b81e8c1fdf5e444f81cb6
james <james.van2019@outlook.com>
2019-08-14 15:21:34 +0800

    every time pack max 1000 transactions and max 5000 sign scripts

--- symbols/qitmeer/transaction.go
+++ symbols/qitmeer/transaction.go
@@ -1,7 +1,15 @@
 package qitmeer
 
-import "github.com/HalalChain/qitmeer-lib/common/hash"
+import (
+	"bytes"
+	"encoding/hex"
+	"github.com/HalalChain/qitmeer-lib/common/hash"
+	"github.com/HalalChain/qitmeer-lib/core/message"
+	"github.com/HalalChain/qitmeer-lib/core/protocol"
+)
 
+const MAX_SIG_COUNT = 5000
+const MAX_TX_COUNT = 1000
 type ParentItems struct {
 	Hash hash.Hash `json:"hash"`
 	Data string    `json:"data"`
@@ -22,3 +30,12 @@ func (p Transactionses) Less(i, j int) bool {
 	return p[i].Fee > p[j].Fee
 }
 func (p Transactionses) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+
+func (this *Transactions) GetSigCount() int{
+	txBytes,_ := hex.DecodeString(this.Data)
+	var mtx *message.MsgTx
+	mtx = new(message.MsgTx)
+	_ = mtx.Decode(bytes.NewReader(txBytes),protocol.ProtocolVersion)
+	return len(mtx.Tx.TxOut)
+}
