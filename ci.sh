cb7f09eedff43bd3518688aa3f8e4e795d916868
james <james.van2019@outlook.com>
2019-11-08 19:04:48 +0800

    modify ci

---
+++ ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+export GO111MODULE=on
+export LD_LIBRARY_PATH=`pwd`/lib/cuckoo/target/release:`pwd`/lib/opencl/linux:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+go mod tidy
+
+if [ ! -x "$(type -p golangci-lint)" ]; then
+  exit 1
+fi
+
+golangci-lint --version
+go build
+echo -e "\n Success!"
