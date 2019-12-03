8d790aee89d7b703e9913c270feb4a2882649dbf
james <james.van2019@outlook.com>
2019-12-04 00:13:01 +0800

    fix

--- lib/cuda/mean.cu
+++ lib/cuda/mean.cu
@@ -718,7 +718,6 @@ int run_solver(SolverCtx* ctx,
 	 print_log("Reason: %s\n", LAST_ERROR_REASON);
 	 return 0;
 	}
-    u64 start = timestamp();
 	for (u32 r = 0; r < range; r++) {
 	if(ctx->trimmer.abort){
 	    break;
@@ -729,9 +728,8 @@ int run_solver(SolverCtx* ctx,
 	 time1 = timestamp();
 	 timems = (time1 - time0) / 1000000;
 	 average[0] = 1000.00/(double)timems;
-	 u64 end = (time1-start) / 1000000 ;
-	 if(end%5000 < 10){ //every 5 second
-	    print_log("\n************** [info] # %d HashRate:%f **************\n",device_id,average[0]);
+	 if(time1/1000000 % 5){ //every 5 second
+	    print_log("\n************** [info] # %d HashRate:%f GPS **************\n",device_id,average[0]);
 	 }
 	 bool isFound = false;
 	 for (unsigned s = 0; s < nsols; s++) {
