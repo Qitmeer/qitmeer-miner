722078402e29cb49a6b17a2613a4989b34205c6e
james <james.van2019@outlook.com>
2019-12-30 23:49:21 +0800

    fix

--- installLibrary.sh
+++ installLibrary.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-rm -rf lib/cuckoo/target/*
-rm -rf libcuckoo.zip
 
-wget -O libcuckoo.zip https://github.com/Qitmeer/cuckoo-lib/releases/download/v0.0.1/libcuckoo.zip
-unzip libcuckoo.zip -d lib/cuckoo/target/
+if [ ! -f "libcuckoo.zip" ]; then
+    rm -rf lib/cuckoo/target/* libcuckoo.zip
+    wget -O libcuckoo.zip https://github.com/Qitmeer/cuckoo-lib/releases/download/v0.0.1/libcuckoo.zip
+    unzip libcuckoo.zip -d lib/cuckoo/target/
+fi
+if [ ! -f "cuckoo.dll" ]; then
+    rm -rf libcuckoo.zip libcudacuckoo.so libcuckoo.so cuckoo.dll cudacuckoo.dll libcuckoo.dylib
+    wget https://github.com/Qitmeer/cuckoo-lib/releases/download/v1.0.0/qitmeer-miner-lib.zip
+    unzip qitmeer-miner-lib.zip
+fi
